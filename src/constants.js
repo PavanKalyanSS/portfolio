3c98bd2ab7ac079bd06d6c2063b27543514a267d
Pavan Kalyan S <pavankalyan36247@gmail.com>
2024-07-15 22:05:07 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,49 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Pavan Kalyan';
+
+export const experiences = [
+    {
+        'company': 'Niosys',
+        'role': 'Front end React js developer',
+        'duration': 'October 2023 - Present',
+        'logo': 'https://www.niosys.net/images/logo-158x40.png',
+        'points': [
+           "- Developing multiple shared components to streamline tasks for fellow developers, enhancing efficiency and collaboration.",
+            "- Contributing significantly to backend development, optimizing essential components for improved product performance.",
+            "- Implementing robust data security measures to ensure role-based access control for the company’s products."
+        ],
+        'url': 'https://www.niosys.net/index.html',
+    },
+    // {
+    //     'company': 'AT & T',
+    //     'role': 'Technical Intern',
+    //     'duration': 'Jan 2023 - March 2023',
+    //     'logo': 'https://www.att.com/ecms/dam/att/consumer/global/logos/att_globe_500x500.jpg',
+    //     'points': [
+    //         'Gained experience with Git version control contributing to the development and maintenance of code repositories.',
+    //         'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.'
+    //     ],
+    //     'url': 'https://www.att.com/',
+    // },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87re';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86';
